d87a08240c0fdc54f36436815701cf27b86961ce
dshvedov <zzz@intome.ru>
2018-05-19 01:53:11 +0300

    Formatting, comments

--- app/src/main/java/ru/d9d/muzen/Track.java
+++ app/src/main/java/ru/d9d/muzen/Track.java
@@ -1,5 +1,7 @@
 package ru.d9d.muzen;
 
+import java.util.Locale;
+
 public class Track {
     /**
      * Track title
@@ -17,55 +19,67 @@ public class Track {
      * Track artwork (cover) image
      */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
-    private static final int NO_IMAGE_PROVIDED=-1;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     /**
      * Create a new track object
      *
-     * @param title
-     * @param artist
-     * @param trackLength
+     * @param title       - track title
+     * @param artist      - track artist
+     * @param trackLength - track length (in seconds)
      */
-    public Track(String title, String artist, int trackLength){
-       mTitle=title;
-       mArtist=artist;
-       mTrackLength=trackLength;
+    public Track(String title, String artist, int trackLength) {
+        mTitle = title;
+        mArtist = artist;
+        mTrackLength = trackLength;
     }
-    public Track(String title, String artist, int trackLength, int imageResourceId){
-        mTitle=title;
-        mArtist=artist;
-        mTrackLength=trackLength;
-        mImageResourceId=imageResourceId;
+
+    /**
+     * Create a new track object
+     *
+     * @param title           - track title
+     * @param artist          - track artist
+     * @param trackLength     - track length (in seconds)
+     * @param imageResourceId - image resource Id
+     */
+    public Track(String title, String artist, int trackLength, int imageResourceId) {
+        mTitle = title;
+        mArtist = artist;
+        mTrackLength = trackLength;
+        mImageResourceId = imageResourceId;
     }
 
     public String getArtist() {
         return mArtist;
     }
+
     public String getTitle() {
         return mTitle;
     }
+
     public int getImageId() {
         return mImageResourceId;
     }
 
     /**
-     * Return track length, converted into hours:minutes:seconds format
-     * @return
+     * Return track length as time string
+     *
+     * @return String format hours:minutes:seconds
      */
     public String getTrackLengthString() {
         int hours = mTrackLength / 3600;
         int minutes = (mTrackLength % 3600) / 60;
         int seconds = mTrackLength % 60;
-        String timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-        return timeString;
+        return String.format(Locale.US, "%02d:%02d:%02d", hours, minutes, seconds);
     }
 
     /**
      * Check if track has cover image associated
-     * @return
+     *
+     * @return boolean value
      */
     public boolean hasImage() {
-        return mImageResourceId!=NO_IMAGE_PROVIDED;
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 
 }
